aabdf029ca08d1c4f0dbc85dfb77b659f85cbef9
Andrew Cawood <cawooda@gmail.com>
2024-05-27 19:14:22 +1000

    fixed

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,8 +1,40 @@
 const router = require('express').Router();
 
+const siteTitle = 'Tech Talk';
+
 router.get('/', async (req,res) => {   
         console.log('home route reached, should call home layout');
-        res.status(200).render('home');
+        return res.render('home', { 
+            siteTitle: siteTitle,
+            pageTitle:"Home Page",
+        });
     });
+
+router.get('/about', async (req,res) => {   
+    console.log('home route reached, should call about layout');
+    return res.render('about', {
+        siteTitle: siteTitle, 
+        pageTitle:"About Page",
+    });
+});
+
+router.get('/contact', async (req,res) => {   
+    console.log('home route reached, should call contact layout');
+    return res.render('contact', { 
+        siteTitle: siteTitle,
+        pageTitle:"Contact Page",
+    });
+});
+
+router.get('/blog', async (req,res) => {   
+    console.log('home route reached, should call blog layout');
+    const blogData = await Blog.findAll({raw:true});
+    //Blog.findByPk(req.params.id,{raw:true});
+    return res.render('blog', { 
+        siteTitle: siteTitle,
+        pageTitle:"Blog Page",
+    });
+});
+
 
 module.exports = router;
