c2a85f488904f4a1fa8c78cd352159b0db17e28c
Andrew Cawood <cawooda@gmail.com>
2024-06-12 12:17:43 +1000

    sdf

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 let sequelize;
 
 if (process.env.DB_URL) {
-	sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
-	sequelize = new Sequelize(
-		process.env.DB_NAME,
-		process.env.DB_USER,
-		process.env.DB_PASSWORD,
-		{
-			host: 'localhost',
-			dialect: 'postgres',
-		},
-	);
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "postgres",
+    }
+  );
 }
 
 module.exports = sequelize;
